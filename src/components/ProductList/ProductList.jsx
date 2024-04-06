51d4463eb3bf01c52dd8bf896090160cb98b3278
Maz Radwan <maz.radwan@keyin.com>
2024-04-06 10:12:11 -0230

    update product list

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,40 +1,47 @@
-import { useState } from "react"
-import { getProducts } from "../../api"
-import ProductDetails from "../ProductDetails/ProductDetails"
-import styles from "./ProductList.module.css"
-import img from "../../assets/SSshavingCream.PNG"
-import img2 from "../../assets/SSbalm.PNG"
-import img3 from "../../assets/SSBlueRazor.PNG"
-import img4 from "../../assets/SSshaveGel.PNG"
+import React from "react"; // Now you only need React import
+import { getProducts } from "../../api";
+import styles from "./ProductList.module.css";
+import img from "../../assets/SSshavingCream.PNG";
+import img2 from "../../assets/SSbalm.PNG";
+import img3 from "../../assets/SSBlueRazor.PNG";
+import img4 from "../../assets/SSshaveGel.PNG";
 
-const allImgs = [img, img2, img3, img4]
+const allImgs = [img, img2, img3, img4];
 
-const ProductList = () => {
-  const products = getProducts()
-  const [selectedProduct, setProduct] = useState(null)
+const ProductList = ({ onProductSelect }) => {
+  // Destructure the passed prop here
+  const products = getProducts();
 
-  const handleModalOpen = (product) => {
-    setProduct(product)
-  }
+  // You don't need selectedProduct state here anymore
 
   return (
     <div className={styles.productContainer}>
       <div className={styles.list}>
         {products.map((product, index) => (
           <div key={product.id} className={styles.product}>
-            <img src={allImgs[index]} width={"200px"} height={"500px"} alt={product.name} />
+            <img
+              src={allImgs[index]}
+              width={"200px"}
+              height={"500px"}
+              alt={product.name}
+            />
             <br />
             {product.name}
             <br />
             {product.price}
             <br />
-            <button onClick={() => handleModalOpen(product)} className={styles.btn}>open Modal</button>
+            <button
+              onClick={() => onProductSelect(product)} // Use the prop function to handle modal opening
+              className={styles.btn}
+            >
+              Open Modal
+            </button>
           </div>
         ))}
       </div>
-      {selectedProduct && <ProductDetails product={selectedProduct} onClose={() => setProduct(null)} />}
+      {/* Removed ProductDetails rendering */}
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
