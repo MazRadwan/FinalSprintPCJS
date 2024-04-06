af85d11887e9f4105972ad3a1d5da9793932ebbe
Evan <evan.pittman@keyin.com>
2024-04-06 10:56:24 -0230

    changed css

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 const products = [
-    { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 10},
-    { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 20},
-    { id: 3, name: 'Product 3', description: 'Description of Product 3', price: 30},
-    { id: 4, name: 'Product 4', description: 'Description of Product 4', price: 40}
-  ];
-  
-  export const getProducts = () => products;
-  
-  export const getProductById = (productId) =>
-    products.find((product) => product.id === productId);
-  
+  { id: 1, name: 'Shave Gel', description: 'Description of Product 1', price: "$ 10.99 CAD"},
+  { id: 2, name: 'Post-Shave Balm', description: 'Description of Product 2', price: "$ 27.99 CAD"},
+  { id: 3, name: 'Premium Shaver  ', description: 'Description of Product 3', price: "$ 19.99 CAD"},
+  { id: 4, name: 'Shave Cream', description: 'Description of Product 4', price: "$ 9.99 CAD"}
+];
+
+export const getProducts = () => products;
+
+export const getProductById = (productId) =>
+  products.find((product) => product.id === productId);
