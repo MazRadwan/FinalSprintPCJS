b180606490020e906cfe1e98b7f0f7a4418671e3
Maz Radwan <maz.radwan@keyin.com>
2024-04-09 21:17:44 -0230

    added confirmation actions and modal

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,17 +2,14 @@ import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-// Create a provider component-for the cart
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
   // Function to add an item to the cart
   const addToCart = (product) => {
     setCartItems((prevItems) => {
-      // Check if the product is already in the cart
       const itemIndex = prevItems.findIndex((item) => item.id === product.id);
       if (itemIndex > -1) {
-        // If it is, update the quantity
         const newItems = [...prevItems];
         newItems[itemIndex].quantity += 1;
         return newItems;
@@ -22,14 +19,12 @@ export const CartProvider = ({ children }) => {
     });
   };
 
-  // Function to remove an item from the cart
   const removeFromCart = (productId) => {
     setCartItems((prevItems) =>
       prevItems.filter((item) => item.id !== productId)
     );
   };
 
-  // Function to increment the quantity of an item in the cart
   const incrementQuantity = (productId) => {
     setCartItems((prevItems) => {
       return prevItems.map((item) => {
@@ -41,7 +36,6 @@ export const CartProvider = ({ children }) => {
     });
   };
 
-  // Function to decrement the quantity of an item in the cart
   const decrementQuantity = (productId) => {
     setCartItems((prevItems) => {
       return prevItems.reduce((acc, item) => {
